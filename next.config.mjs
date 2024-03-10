30334eff874579bffe81bc81ef9af65708725ea6
flparvez <flparvez23@gmail.com>
2024-03-10 13:36:57 +0600

    production fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
     images: {
-        domains: ['images.unsplash.com']
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+            },
+            {
+                protocol: "https",
+                hostname: "digitalhippo-production.up.railway.app",
+            },
+        ],
     },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
